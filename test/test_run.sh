ea067524591263e20283bd7320e9167951990fa9
Mohammad Sahraeian <mohammad@tehran-07.binatechnologies.com>
2015-10-13 18:13:35 -0700

    few

--- test/test_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-METASV="run_metasv.py"
-$METASV --pindel_native pindel/22_* --breakdancer_native breakdancer/22.out --cnvnator_native cnvnator/22.out --breakseq_native breakseq/breakseq.gff --reference reference/human_g1k_v37_decoy.fasta --outdir out --sample NA12878 --disable_assembly --filter_gaps --keep_standard_contigs
-
